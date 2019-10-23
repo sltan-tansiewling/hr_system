ccc9b2458b12098ce4ebf4f1137a9a8284d44e90
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-10-23 23:48:44 +0800

    Added method to get leave application by ID

--- controllers/hr.js
+++ controllers/hr.js
@@ -8,7 +8,7 @@ module.exports = (db) => {
 
     let getAllStaffLeaveApplication = (request, response) => {
 
-        db.hr.getStaffLeaveApplications ((error, leaveApplications) => {
+        db.hr.getStaffLeaveApplications((error, leaveApplications) => {
 
             if (error) {
                 console.log("Error occurred");
@@ -29,12 +29,38 @@ module.exports = (db) => {
         });
     };
 
+    let getLeaveApplicationDetailsById = (request, response) => {
+
+        let selectedLeaveApplicationId = [request.params.id];
+
+        db.hr.getStaffLeaveApplicationById(selectedLeaveApplicationId, (error, leaveApplicationDetails) => {
+
+            if (error) {
+                console.log("Error occurred");
+            } else {
+
+                if (leaveApplicationDetails.length > 0) {
+
+                    const data = {
+                        records: leaveApplicationDetails
+                    };
+                    response.render('hr/leaveApplicationById', data);
+
+                } else {
+                    response.send("There are no leave applications with this ID.");
+                }
+
+            }
+        });
+    };
+
     /**
      * ===========================================
      * Export controller functions as a module
      * ===========================================
      */
     return {
-        getAllLeaveApplication: getAllStaffLeaveApplication
+        getAllLeaveApplication: getAllStaffLeaveApplication,
+        getLeaveApplicationById: getLeaveApplicationDetailsById
     };
 };
