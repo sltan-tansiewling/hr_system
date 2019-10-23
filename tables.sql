cf7ad6d62ee6863d73b86600a7d898a647a56a56
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-10-23 09:51:50 +0800

    Added tables.sql

---
+++ tables.sql
@@ -0,0 +1,31 @@
+CREATE TABLE IF NOT EXISTS role_type (
+    id SERIAL PRIMARY KEY,
+    name TEXT
+);
+
+CREATE TABLE IF NOT EXISTS leave_type (
+    id SERIAL PRIMARY KEY,
+    name TEXT
+);
+
+CREATE TABLE IF NOT EXISTS leave_status (
+    id SERIAL PRIMARY KEY,
+    name TEXT
+);
+
+CREATE TABLE IF NOT EXISTS employee (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    dept TEXT,
+    join_since DATE,
+    role_type_id INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS leave_application (
+    id SERIAL PRIMARY KEY,
+    employee_id INTEGER,
+    leave_type_id INTEGER,
+    start_date DATE,
+    end_date DATE,
+    leave_status_id INTEGER
+);
