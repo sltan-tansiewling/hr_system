1d96f0a7748989929e5f89e8dc958a9e63f44a58
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-10-23 23:49:08 +0800

    Added query for single leave application

--- models/hr.js
+++ models/hr.js
@@ -12,20 +12,33 @@ module.exports = (dbPoolInstance) => {
         dbPoolInstance.query(getStaffLeaveApplicationsQuery, (error, queryResult) => {
 
             if (error) {
-
                 console.log("There is an error querying for leave application.");
-
                 callback(error, null);
-
             } else {
-
                 callback(null, queryResult.rows);
+            }
+        });
+    };
 
+    let getStaffLeaveApplicationById = (id, callback) => {
+
+        let selectedId = id;
+        let getStaffLeaveIdQuery = "SELECT leave_application.id, employees.name AS employee_name, leave_type.name AS leave_type, leave_application.start_date, leave_application.end_date, leave_status.name AS status FROM leave_application INNER JOIN employees ON (leave_application.employee_id = employees.id) INNER JOIN leave_type ON (leave_application.leave_type_id = leave_type.id) INNER JOIN leave_status ON (leave_application.leave_status_id = leave_status.id) WHERE leave_application.id = $1";
+
+        dbPoolInstance.query(getStaffLeaveIdQuery, selectedId, (error, queryResult) => {
+
+            console.log(selectedId);
+            if (error) {
+                console.log("There is an error querying for leave application by id.");
+                callback(error, null);
+            } else {
+                callback(null, queryResult.rows);
             }
         });
     };
 
     return {
-        getStaffLeaveApplications
+        getStaffLeaveApplications,
+        getStaffLeaveApplicationById
     };
 };
