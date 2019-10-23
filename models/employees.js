15f4361cf7bb5ebac986f468d6f53d04e82b77d2
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-10-23 14:35:13 +0800

    Added method to check role type of the user logging in

--- models/employees.js
+++ models/employees.js
@@ -5,7 +5,25 @@
  */
 module.exports = (dbPoolInstance) => {
 
-    return {
+    let checkUserRole = (username, callback) => {
 
+        let userInputUsername = [username];
+        console.log("In model, username is " + userInputUsername);
+
+        let checkUserRoleQuery = "SELECT role_type_id from employees WHERE username = $1";
+
+        dbPoolInstance.query(checkUserRoleQuery, userInputUsername, (error, queryResult) => {
+
+            if (error) {
+                console.log("There is error querying for user role.");
+                console.log(error.message);
+            } else {
+                callback(null, queryResult);
+            }
+        });
+    };
+
+    return {
+        checkUserRole
     };
 };
