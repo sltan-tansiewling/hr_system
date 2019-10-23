a095fb7a5b80d190197603d7c5973ac7760fadcd
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-10-23 10:35:45 +0800

    Updated table name

--- seed.sql
+++ seed.sql
@@ -8,7 +8,7 @@ INSERT INTO leave_status (name) VALUES ('Pending Approval');
 INSERT INTO leave_status (name) VALUES ('Approved');
 INSERT INTO leave_status (name) VALUES ('Rejected');
 
-INSERT INTO employee (name, dept, join_since, role_type_id) VALUES ('Michelle', 'Manpower', '2010-01-04', 1);
-INSERT INTO employee (name, dept, join_since, role_type_id) VALUES ('Siew Ling', 'IT', '2019-09-16', 2);
+INSERT INTO employees (name, dept, join_since, role_type_id) VALUES ('Michelle', 'Manpower', '2010-01-04', 1);
+INSERT INTO employees (name, dept, join_since, role_type_id) VALUES ('Siew Ling', 'IT', '2019-09-16', 2);
 
 INSERT INTO leave_application (employee_id, leave_type_id, start_date, end_date, leave_status_id) VALUES (2, 1, '2019-12-09', '2019-12-31', 1);
