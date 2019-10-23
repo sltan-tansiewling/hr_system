eb734ac2bdbf67e40b6bab49389bf8635aa61add
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-10-23 23:50:23 +0800

    Added view for single application details

---
+++ views/hr/leaveApplicationById.jsx
@@ -0,0 +1,35 @@
+var React = require('react');
+
+class LeaveApplication extends React.Component {
+
+    render () {
+
+        return (
+            <html>
+            <head>
+                <title>HR - View Staff Leave Application Details</title>
+            </head>
+            <body>
+                <h1>Staff Leave Application Details</h1>
+                <form method="GET">
+                    ID: <input type="text" name="id" value={this.props.records[0].id} readOnly />
+                    <br />
+                    Employee: <input type="text" name="employee_name" value={this.props.records[0].employee_name} readOnly />
+                    <br />
+                    Leave Type: <input type="text" name="leave_type" value={this.props.records[0].leave_type} readOnly />
+                    <br />
+                    Start Date: <input type="text" name="start_date" value={this.props.records[0].start_date} readOnly />
+                    <br />
+                    End Date: <input type="text" name="end_date" value={this.props.records[0].end_date} readOnly />
+                    <br />
+                    Status: <input type="text" name="status" value={this.props.records[0].status} readOnly />
+                    <br />
+                    <input type="submit" value="Edit" />
+                </form>
+            </body>
+            </html>
+        );
+    };
+};
+
+module.exports = LeaveApplication;
