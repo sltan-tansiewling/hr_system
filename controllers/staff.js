164a9878a632b79bd14f50d395448a35bb4ca87d
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-10-24 15:41:47 +0800

    Added methods for getting and creating new leave application

--- controllers/staff.js
+++ controllers/staff.js
@@ -5,7 +5,33 @@ module.exports = (db) => {
      * Controller logic
      * ===========================================
      */
+     let getNewLeaveApplicationForm = (request,response) => {
+        response.render('staff/applyLeave');
+     };
 
+     let createLeaveApplication = (request, response) => {
+
+        // Read cookie user ID
+        let currentUserId = request.cookies["userId"];
+        console.log("Read user id from request cookies " + currentUserId);
+
+        let newLeaveValues = {
+            employeeId: currentUserId,
+            leaveType: request.body.leave_type,
+            startDate: request.body.start_date,
+            endDate: request.body.start_date,
+            defaultStatus: 1
+        };
+
+        db.staff.createNewLeaveApplication (newLeaveValues, (error, queryResult) => {
+
+            if (error) {
+                console.log("Controller: ", error.message);
+            } else {
+                response.send(newLeaveValues);
+            }
+        });
+     };
 
 
     /**
@@ -14,6 +40,7 @@ module.exports = (db) => {
      * ===========================================
      */
     return {
-
+        getNewLeaveApplicationForm,
+        createLeaveApplication
     };
 };
