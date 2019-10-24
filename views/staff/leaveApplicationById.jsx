c9764ee92cf70b82021eae1dd947b799d8d84e61
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-10-24 17:03:03 +0800

    Added new view to display leave application details

---
+++ views/staff/leaveApplicationById.jsx
@@ -0,0 +1,32 @@
+var React = require('react');
+
+class LeaveApplication extends React.Component {
+
+    render () {
+
+        return (
+            <html>
+            <head>
+                <title>Staff - View Leave Application Details</title>
+            </head>
+            <body>
+                <h1>Leave Application Details</h1>
+                <form method="GET">
+                    ID: <input type="text" name="id" value={this.props.records[0].id} readOnly />
+                    <br />
+                    Leave Type: <input type="text" name="leave_type" value={this.props.records[0].leave_type} readOnly />
+                    <br />
+                    Start Date: <input type="text" name="start_date" value={this.props.records[0].start_date} readOnly />
+                    <br />
+                    End Date: <input type="text" name="end_date" value={this.props.records[0].end_date} readOnly />
+                    <br />
+                    Status: <input type="text" name="status" value={this.props.records[0].status} readOnly />
+                </form>
+                <a href={"/staff/leaveApplication/" + this.props.records[0].id + "/edit"}>Edit</a>
+            </body>
+            </html>
+        );
+    };
+};
+
+module.exports = LeaveApplication;
