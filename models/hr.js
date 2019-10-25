1fbc830c143737986de9b17ec931f62179c1f2d3
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-10-25 09:48:43 +0800

    Updated query for view all applications to order by ID in descending order

--- models/hr.js
+++ models/hr.js
@@ -7,7 +7,7 @@ module.exports = (dbPoolInstance) => {
 
     let getStaffLeaveApplications = (callback) => {
 
-        let getStaffLeaveApplicationsQuery = "SELECT leave_application.id, employees.name AS employee_name, leave_type.name AS leave_type, leave_application.start_date, leave_application.end_date, leave_status.name AS status FROM leave_application INNER JOIN employees ON (leave_application.employee_id = employees.id) INNER JOIN leave_type ON (leave_application.leave_type_id = leave_type.id) INNER JOIN leave_status ON (leave_application.leave_status_id = leave_status.id)";
+        let getStaffLeaveApplicationsQuery = "SELECT leave_application.id, employees.name AS employee_name, leave_type.name AS leave_type, leave_application.start_date, leave_application.end_date, leave_status.name AS status FROM leave_application INNER JOIN employees ON (leave_application.employee_id = employees.id) INNER JOIN leave_type ON (leave_application.leave_type_id = leave_type.id) INNER JOIN leave_status ON (leave_application.leave_status_id = leave_status.id) ORDER BY leave_application.id DESC";
 
         dbPoolInstance.query(getStaffLeaveApplicationsQuery, (error, queryResult) => {
 
