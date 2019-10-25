f58b824a42d67e0b63772d5f85e3864a290f92c7
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-10-25 09:26:09 +0800

    Added navbar component for staff

---
+++ views/layouts/staff_navbar.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+
+class StaffNavbar extends React.Component {
+
+    render () {
+
+        return (
+
+            <div>
+                <ul>
+                    <li>
+                        <a href="/staff/leaveApplication">
+                            View All Leave Applications
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/staff/leaveApplication/new">
+                            New Leave Application
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        );
+    };
+};
+
+module.exports = StaffNavbar;
