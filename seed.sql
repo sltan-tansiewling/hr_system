12ee0c4d79881c0f09df5a047cea3775fc970f0a
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-10-30 00:13:39 +0800

    Added another sample data for user

--- seed.sql
+++ seed.sql
@@ -11,5 +11,6 @@ INSERT INTO leave_status (name) VALUES ('Cancel');
 
 INSERT INTO employees (name, dept, join_since, role_type_id, username, password) VALUES ('Michelle', 'Manpower', '2010-01-04', 1, 'michellelim', 'mlpwd');
 INSERT INTO employees (name, dept, join_since, role_type_id, username, password) VALUES ('Siew Ling', 'IT', '2019-09-16', 2, 'sltan', 'slpwd');
+INSERT INTO employees (name, dept, join_since, role_type_id, username, password) VALUES ('Elaine', 'PM', '2019-05-02', 2, 'neoel', 'elpwd');
 
 INSERT INTO leave_application (employee_id, leave_type_id, start_date, end_date, leave_status_id) VALUES (2, 1, '2019-12-09', '2019-12-31', 1);
