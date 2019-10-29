73321168cc721c9eb6e9e3cc7af9850e80dfb2aa
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-10-29 11:28:18 +0800

    Updated create new application to get leave type from input and added a new method to query for all leave type to populate in drop down list

--- models/staff.js
+++ models/staff.js
@@ -24,46 +24,19 @@ module.exports = (dbPoolInstance) => {
 
     let createNewLeaveApplication = (inputs, callback) => {
 
-        let leaveType = [inputs.leaveType];
-        console.log("Model: Leave Type is " + leaveType);
+        let newLeave = [inputs.employeeId, inputs.leaveTypeId, inputs.startDate, inputs.endDate, inputs.defaultStatus];
 
-        let leaveTypeIdQuery = "SELECT id FROM leave_type WHERE name = $1";
+        let createLeaveQuery = "INSERT INTO leave_application (employee_id, leave_type_id, start_date, end_date, leave_status_id) VALUES ($1, $2, $3, $4, $5)";
 
-        dbPoolInstance.query(leaveTypeIdQuery, leaveType, (error, queryResult) => {
+        dbPoolInstance.query(createLeaveQuery, newLeave, (error, queryResult) => {
 
             if (error) {
-                callback (error.message, null);
+                console.log("There is an error creating new leave application.");
+                console.log(error.message);
+                callback(error, null);
             } else {
-                console.log(queryResult.rows[0].id);
-
-                console.log("Model inputs: ", inputs);
-                let employeeId = inputs.employeeId;
-
-                let leaveTypeId = queryResult.rows[0].id;
-
-                let startDate = inputs.startDate;
-                console.log("Model: Start Date is " + startDate);
-
-                let endDate = inputs.endDate;
-                console.log("Model: End Date is ", endDate);
-
-                let defaultStatus = inputs.defaultStatus;
-
-                let newLeave = [employeeId, leaveTypeId, startDate, endDate, defaultStatus];
-
-                let createLeaveQuery = "INSERT INTO leave_application (employee_id, leave_type_id, start_date, end_date, leave_status_id) VALUES ($1, $2, $3, $4, $5)";
-
-                dbPoolInstance.query(createLeaveQuery, newLeave, (error, queryResult) => {
-
-                    if (error) {
-                        console.log("There is an error creating new leave application.");
-                        console.log(error.message);
-                        callback(error, null);
-                    } else {
-                        callback(null, queryResult.rows);
-                        console.log("Model: Create successful!");
-                    }
-                });
+                callback(null, queryResult.rows);
+                console.log("Model: Create successful!");
             }
         });
     };
@@ -137,6 +110,20 @@ module.exports = (dbPoolInstance) => {
         });
     };
 
+    let getLeaveType = (callback) => {
+
+        let getLeaveTypeQuery = "SELECT * FROM leave_type";
+
+        dbPoolInstance.query(getLeaveTypeQuery, (error, queryResult) => {
+
+            if (error) {
+                callback(error, null);
+            } else {
+                callback(null, queryResult.rows);
+            }
+        });
+    }
+
     /*let getLeaveEntitlement = (userId, callback) => {
 
         // Get current user ID
@@ -183,6 +170,7 @@ module.exports = (dbPoolInstance) => {
         getOwnLeaveApplications,
         createNewLeaveApplication,
         getLeaveApplicationDetailsById,
-        updateLeaveApplicationDetailsById
+        updateLeaveApplicationDetailsById,
+        getLeaveType
     };
 };
