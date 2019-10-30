b1f315760aa69dbf58aa30abcf46341880257e54
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-10-30 15:30:29 +0800

    Debugging return props values

--- views/staff/allLeaveApplication.jsx
+++ views/staff/allLeaveApplication.jsx
@@ -5,21 +5,21 @@ var GlobalNavbar = require('../layouts/global_navbar');
 
 class AllLeaveApplication extends React.Component {
     render() {
-let displayRecords = "Hello";
-        // let displayRecords = this.props.records.map ((application) => {
 
-        //     return (
-        //         <tr>
-        //             <td>{application.id}</td>
-        //             <td>{application.leave_type}</td>
-        //             <td>{application.start_date}</td>
-        //             <td>{application.end_date}</td>
-        //             <td>{application.status}</td>
-        //             <td><a href={"/staff/leaveApplication/" + application.id}>View</a></td>
-        //             <td><a href={"/staff/leaveApplication/" + application.id + "/edit"}>Edit</a></td>
-        //         </tr>
-        //     );
-        // });
+        let displayRecords = this.props.records.map ((application) => {
+
+            return (
+                <tr>
+                    <td>{application.id}</td>
+                    {/*<td>{application.leave_type}</td>
+                    <td>{application.start_date}</td>
+                    <td>{application.end_date}</td>
+                    <td>{application.status}</td>
+                    <td><a href={"/staff/leaveApplication/" + application.id}>View</a></td>
+                    <td><a href={"/staff/leaveApplication/" + application.id + "/edit"}>Edit</a></td>*/}
+                </tr>
+            );
+        });
 
         return (
 
