eba8ab3728c1d694b3daa8df4625d1ccfe05c05a
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-10-30 15:57:31 +0800

    Troubleshooting on heroku

--- controllers/staff.js
+++ controllers/staff.js
@@ -19,6 +19,8 @@ module.exports = (db) => {
                     records: queryResult
                 };
 
+                console.log(data.records);
+
                 response.render('staff/allLeaveApplication', data);
             }
         });
